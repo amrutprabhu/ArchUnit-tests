db0c86258f6e2e4234c3f3f6f2dfd36aae0f51b8
Amrut Prabhu <amrut.prabhu@raisin.com>
2019-03-10 18:35:47 +0100

    Added more tests to try package level seperation.

--- ArchUnittests/src/test/java/com/amrut/spring/controller/UserControllerTest.java
+++ ArchUnittests/src/test/java/com/amrut/spring/controller/UserControllerTest.java
@@ -1,18 +1,18 @@
 package com.amrut.spring.controller;
 
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.junit.ArchUnitRunner;
 import com.tngtech.archunit.lang.ArchRule;
-
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-
 import org.junit.runner.RunWith;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
 @RunWith(ArchUnitRunner.class)
-@AnalyzeClasses(packages = "com.amrut.spring")
+@AnalyzeClasses(packages = "com.amrut.spring",importOptions = {ImportOption.DontIncludeTests.class})
 public class UserControllerTest {
 
     @ArchTest
@@ -35,6 +35,15 @@ public class UserControllerTest {
                 .areAnnotatedWith(RestController.class)
             .should()
                 .haveSimpleNameNotEndingWith("Service");
+
+
+    @ArchTest
+    public static final ArchRule service_should_notaccess_controller = noClasses()
+            .that()
+                .resideInAPackage("com.amrut.spring.service")
+            .should()
+                .accessClassesThat()
+                .resideInAPackage("com.amrut.spring.controller");
 
 
 }
